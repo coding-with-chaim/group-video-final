5e2710b7dbd6077a085947a1a7c08b04029e4ac0
Jiwon <chloe326o.o@gmail.com>
2020-07-23 20:15:44 +0900

    Standard format applied

--- server.js
+++ server.js
@@ -1,105 +1,105 @@
-require("dotenv").config();
-const express = require("express");
-const http = require("http");
-const app = express();
-const server = http.createServer(app);
-const socket = require("socket.io");
-const io = socket(server);
+require('dotenv').config()
+const express = require('express')
+const http = require('http')
+const app = express()
+const server = http.createServer(app)
+const socket = require('socket.io')
+const io = socket(server)
 
-const testees = {};
-const testers = {};
-const users = {};
-const socketToRoom = {};
+const testees = {}
+const testers = {}
+const users = {}
+const socketToRoom = {}
 
-io.on("connection", socket => {
-  console.log("connection occured");
+io.on('connection', socket => {
+  console.log('connection occured')
   // tester has joined the room
   // make p2p connection with everybody in the room
-  socket.on("tester join room", roomID => {
-    console.log("Tester has joined the room!", roomID);
+  socket.on('tester join room', roomID => {
+    console.log('Tester has joined the room!', roomID)
 
     if (users[roomID]) {
-      const length = testers[roomID].length;
+      const length = testers[roomID].length
       if (length === 4) {
-        console.log("Tester room full");
-        socket.emit("room full");
+        console.log('Tester room full')
+        socket.emit('room full')
         return;
       }
 
-      users[roomID].push(socket.id);
+      users[roomID].push(socket.id)
     } else {
-      users[roomID] = [socket.id];
+      users[roomID] = [socket.id]
     }
 
     if (testers[roomID]) {
-      testers[roomID].push(socket.id);
+      testers[roomID].push(socket.id)
     } else {
-      testers[roomID] = [socket.id];
+      testers[roomID] = [socket.id]
     }
 
-    socketToRoom[socket.id] = roomID;
-    console.log(users[roomID], roomID, socket.id);
-    const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
-    console.log("users in this room:", usersInThisRoom);
-    socket.emit("all users", usersInThisRoom);
-  });
+    socketToRoom[socket.id] = roomID
+    console.log(users[roomID], roomID, socket.id)
+    const usersInThisRoom = users[roomID].filter(id => id !== socket.id)
+    console.log('users in this room:', usersInThisRoom)
+    socket.emit('all users', usersInThisRoom)
+  })
   // testee has joined the room
   // only make p2p connection with testers
-  socket.on("join room", roomID => {
-    console.log("User has joined the room!", roomID);
+  socket.on('join room', roomID => {
+    console.log('User has joined the room!', roomID)
 
     if (users[roomID]) {
-      const length = users[roomID].length;
+      const length = users[roomID].length
       if (length === 4) {
-        console.log("Tester room full");
-        socket.emit("room full");
-        return;
+        console.log('Tester room full')
+        socket.emit('room full')
+        return
       }
 
-      users[roomID].push(socket.id);
+      users[roomID].push(socket.id)
     } else {
-      users[roomID] = [socket.id];
+      users[roomID] = [socket.id]
     }
 
     if (testees[roomID]) {
-      testees[roomID].push(socket.id);
+      testees[roomID].push(socket.id)
     } else {
-      testees[roomID] = [socket.id];
+      testees[roomID] = [socket.id]
     }
-    console.log(users[roomID], roomID, socket.id);
-    socketToRoom[socket.id] = roomID;
+    console.log(users[roomID], roomID, socket.id)
+    socketToRoom[socket.id] = roomID
     if (testers[roomID]) {
-      const testersInThisRoom = testers[roomID].filter(id => id !== socket.id);
-      socket.emit("all testers", testersInThisRoom);
+      const testersInThisRoom = testers[roomID].filter(id => id !== socket.id)
+      socket.emit('all testers', testersInThisRoom)
     } else {
-      socket.emit("all testers", []);
+      socket.emit('all testers', [])
     }
-  });
+  })
 
-  socket.on("sending signal", payload => {
-    io.to(payload.userToSignal).emit("user joined", {
+  socket.on('sending signal', payload => {
+    io.to(payload.userToSignal).emit('user joined', {
       signal: payload.signal,
       callerID: payload.callerID
-    });
-  });
+    })
+  })
 
-  socket.on("returning signal", payload => {
-    io.to(payload.callerID).emit("receiving returned signal", {
+  socket.on('returning signal', payload => {
+    io.to(payload.callerID).emit('receiving returned signal', {
       signal: payload.signal,
       id: socket.id
-    });
-  });
+    })
+  })
 
-  socket.on("disconnect", () => {
-    const roomID = socketToRoom[socket.id];
-    let room = users[roomID];
+  socket.on('disconnect', () => {
+    const roomID = socketToRoom[socket.id]
+    let room = users[roomID]
     if (room) {
-      room = room.filter(id => id !== socket.id);
-      users[roomID] = room;
+      room = room.filter(id => id !== socket.id)
+      users[roomID] = room
     }
-  });
-});
+  })
+})
 
 server.listen(process.env.PORT || 8800, () =>
-  console.log("server is running on port 8800")
-);
+  console.log('server is running on port 8800')
+)
