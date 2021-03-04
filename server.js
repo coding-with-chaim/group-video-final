7c490ce34cce51f2d981ec5e0c065bc431cf8c70
Mike Vanis <hello@mikevanis.com>
2021-03-04 17:14:12 +0000

    serve frontend with node

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 require('dotenv').config();
+const path = require("path");
 const express = require("express");
 const http = require("http");
-const app = express();
-const server = http.createServer(app);
 const socket = require("socket.io");
-const io = socket(server);
 
 const users = {};
-
 const socketToRoom = {};
 
+// Serve frontend
+const app = express();
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const server = http.createServer(app);
+
+const io = socket(server);
 io.on('connection', socket => {
     socket.on("join room", roomID => {
         if (users[roomID]) {
@@ -48,5 +55,3 @@ io.on('connection', socket => {
 });
 
 server.listen(process.env.PORT || 8000, () => console.log('server is running on port 8000'));
-
-
