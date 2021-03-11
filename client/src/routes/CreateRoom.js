ce091d0a0e8ef25c649cb29dcf5715280a773e7e
Mike Vanis <hello@mikevanis.com>
2021-03-11 17:26:39 +0000

    human readable id

--- client/src/routes/CreateRoom.js
+++ client/src/routes/CreateRoom.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { v1 as uuid } from "uuid";
+import {getHR, getId} from "reversible-human-readable-id";
 
 const CreateRoom = (props) => {
     function create() {
         const id = uuid();
-        props.history.push(`/room/${id}`);
+        const niceId = getHR(id);
+        props.history.push(`/room/${niceId}`);
     }
 
     return (
